d31f4b65a8a5ca6412d1b1f885a3848077244987
sidhu177 <sidharth177@gmail.com>
2019-04-10 23:10:31 -0400

    Jinja update

--- headlines.py
+++ headlines.py
@@ -4,22 +4,14 @@ from flask import render_template
 
 app = Flask(__name__)
 
-RSS_Feed = {'cbc':"https://www.cbc.ca/cmlink/rss-topstories",'ctv':"https://www.ctvnews.ca/rss/ctvnews-ca-top-stories-public-rss-1.822009"}
+RSS_Feed = {'cbc':"https://www.cbc.ca/cmlink/rss-topstories",
+             'ctv':"https://www.ctvnews.ca/rss/ctvnews-ca-top-stories-public-rss-1.822009"}
 
 @app.route("/")
-@app.route("/cbc")
-def cbc():
-    return get_news('cbc')
-
-@app.route("/ctv")
-def ctv():
-    return get_news('ctv')
-
 @app.route("/<publication>")
 def get_news(publication="cbc"):
     feed = feedparser.parse(RSS_Feed[publication])
-    first_article = feed['entries'][0]
-    return render_template("home.html")
+    return render_template("home.html", articles=feed['entries'])
 
 
 if __name__=='__main__':

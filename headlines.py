5fba4e476382a929d0f2786a25d737abcec50dcd
sidhu177 <sidharth177@gmail.com>
2019-04-11 21:35:20 -0400

    update

--- headlines.py
+++ headlines.py
@@ -1,6 +1,10 @@
 import feedparser
 from flask import Flask
 from flask import render_template
+from flask import request
+import json
+import urllib
+import urllib2
 
 app = Flask(__name__)
 
@@ -13,6 +17,8 @@ def get_news(publication="cbc"):
     feed = feedparser.parse(RSS_Feed[publication])
     return render_template("home.html", articles=feed['entries'])
 
+def get_rate(frm,to):
+    return (to_rate/frm_rate,parsed.keys())
 
 if __name__=='__main__':
     app.run(port=5000)

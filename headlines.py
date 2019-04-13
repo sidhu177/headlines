acfe277b9cdca9ed9355812841ff707f971bfffb
sidhu177 <sidharth177@gmail.com>
2019-04-12 23:33:18 -0400

    REST GET

--- headlines.py
+++ headlines.py
@@ -3,8 +3,6 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
-import urllib
-import urllib2
 
 app = Flask(__name__)
 
@@ -12,13 +10,14 @@ RSS_Feed = {'cbc':"https://www.cbc.ca/cmlink/rss-topstories",
              'ctv':"https://www.ctvnews.ca/rss/ctvnews-ca-top-stories-public-rss-1.822009"}
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication="cbc"):
+def get_news():
+    query = request.args.get("publication")
+    if not query or query.lower() not in RSS_Feed:
+        publication = "cbc"
+    else:
+        publication = query.lower()
     feed = feedparser.parse(RSS_Feed[publication])
     return render_template("home.html", articles=feed['entries'])
-
-def get_rate(frm,to):
-    return (to_rate/frm_rate,parsed.keys())
 
 if __name__=='__main__':
     app.run(port=5000)

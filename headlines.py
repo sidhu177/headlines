97f24a3e3938049fcd72c47b152258deae4a917a
sidhu177 <sidharth177@gmail.com>
2019-04-14 22:26:02 -0400

    Weather API

--- headlines.py
+++ headlines.py
@@ -5,11 +5,17 @@ from flask import request
 import json
 from urllib.request import urlopen
 import urllib
+import urllib.parse
+import urllib.request
 
 app = Flask(__name__)
 
 RSS_Feed = {'cbc':"https://www.cbc.ca/cmlink/rss-topstories",
              'ctv':"https://www.ctvnews.ca/rss/ctvnews-ca-top-stories-public-rss-1.822009"}
+
+weather_url = "http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&APPID=cb932829eacb6a0e9ee4f38bfbf112ed"
+
+DEFAULTS = {'city':'Toronto,Canada'}
 
 @app.route("/")
 def get_news():
@@ -19,7 +25,25 @@ def get_news():
     else:
         publication = query.lower()
     feed = feedparser.parse(RSS_Feed[publication])
-    return render_template("home.html", articles=feed['entries'])
+    city = request.args.get('city')
+    if not city: 
+        city = DEFAULTS['city']
+    weather = get_weather(city)
+    return render_template("home.html", articles=feed['entries'],weather=weather )
+
+def get_weather(query):
+    query = urllib.parse.quote(query)
+    url = weather_url.format(query)
+    data = urllib.request.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get('weather'):
+        weather = {'description':parsed['weather'][0]['description'],
+                   'temperature':parsed['main']['temp'],
+                   'city':parsed['name'],
+                   'country':parsed['sys']['country']
+                   }
+    return weather
 
 if __name__=='__main__':
     app.run(port=5000)
